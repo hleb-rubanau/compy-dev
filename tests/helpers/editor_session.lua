1bf1b1542c4759823275da2989dd537c3e661793
Hleb Rubanau <g.rubanau@gmail.com>
2026-04-14 14:57:07 +0200

    chore(tests/editor_session): autoformat test file
    Conservatively setting col=65 to match editor's own conventions

--- tests/helpers/editor_session.lua
+++ tests/helpers/editor_session.lua
@@ -1,13 +1,15 @@
-local class = require('util.class')
-local assert = require('luassert')
-local fmt = string.format 
+local class = require("util.class")
+local assert = require("luassert")
+local fmt = string.format
 
-EditorSession = class.create( 
-  function(controller, press, save, mock) 
-    return { controller=controller, 
-             press=press, 
-             save=save, 
-             mock=mock } 
+EditorSession = class.create(
+  function(controller, press, save, mock)
+    return {
+      controller = controller,
+      press = press,
+      save = save,
+      mock = mock,
+    }
   end
 )
 
@@ -18,23 +20,27 @@ function EditorSession:open(src, nb)
   local buffer = self.controller:get_active_buffer()
 
   local srclines = string.lines(src)
-  local has_trailing_nl = ( srclines[#srclines] == '' )
-  local expected = (has_trailing_nl and src or src.."\n")
+  local has_trailing_nl = (srclines[#srclines] == "")
+  local expected = (has_trailing_nl and src or src .. "\n")
 
-  assert.same(string.lines(expected),
-              buffer:get_text_content(),
-              "desired content loaded")
+  assert.same(
+    string.lines(expected),
+    buffer:get_text_content(),
+    "desired content loaded"
+  )
   if nb then
     if has_trailing_nl then
-      assert.same(nb,
-                  buffer:get_content_length(),
-                  fmt("Blocks loaded: %s", nb)
-                 )
+      assert.same(
+        nb,
+        buffer:get_content_length(),
+        fmt("Blocks loaded: %s", nb)
+      )
     else
-      assert.same(nb+1,
-                  buffer:get_content_length(),
-                  fmt("Blocks loaded: %s original +1 extra", nb)
-                 )
+      assert.same(
+        nb + 1,
+        buffer:get_content_length(),
+        fmt("Blocks loaded: %s original +1 extra", nb)
+      )
     end
   end
   self.input = input
@@ -45,47 +51,55 @@ end
 -- this helper only works reliably in block-level navigation mode!
 -- (when editor input buffer is not yet activated with <ESC>)
 function EditorSession:select_block(n, target_content)
-
   local s = self.buffer.selection
   local dir = (s > n) and "up" or "down"
-  local steps = math.abs(s-n)
+  local steps = math.abs(s - n)
   if (not self.input:is_empty()) and (steps > 0) then
-    local jumpkey = dir=='up' and 'home' or 'end'
+    local jumpkey = dir == "up" and "home" or "end"
     self.mock.keystroke(jumpkey, self.press)
   end
   for i = 1, steps do
     self.mock.keystroke(dir, self.press)
   end
 
-  assert.same(n, self.buffer.selection,
-              fmt("selection moved to block #%s",n))
+  assert.same(
+    n,
+    self.buffer.selection,
+    fmt("selection moved to block #%s", n)
+  )
   if target_content then
-    assert.same(string.lines(target_content),
-                self.buffer:get_selected_text(),
-                "selected block #%s has expected content", n)
+    assert.same(
+      string.lines(target_content),
+      self.buffer:get_selected_text(),
+      "selected block #%s has expected content",
+      n
+    )
   end
 end
 
 function EditorSession:select_and_open_block(n, target_content)
   self:select_block(n, target_content)
-  self.mock.keystroke('escape', self.press)
+  self.mock.keystroke("escape", self.press)
 
   assert.same(n, self.buffer.loaded, fmt("loaded block #%s", n))
   if target_content then
-    assert.same(string.lines(target_content),
-                self.input:get_text(),
-                "input loaded expected content (block #%s)", n)
+    assert.same(
+      string.lines(target_content),
+      self.input:get_text(),
+      "input loaded expected content (block #%s)",
+      n
+    )
   end
 end
 
 function EditorSession:alter_input(newtext)
   local newlines = string.lines(newtext)
   self.input:set_text(newlines)
-  assert.same( newlines, self.input:get_text(), "input altered")
+  assert.same(newlines, self.input:get_text(), "input altered")
 end
 
 function EditorSession:submit(newtext, ctrl)
   self:alter_input(newtext)
-  local keycode = ctrl and 'C-return' or 'return'
+  local keycode = ctrl and "C-return" or "return"
   self.mock.keystroke(keycode, self.press)
 end
