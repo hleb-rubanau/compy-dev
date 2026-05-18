801f16c7f56c15c23e0fd5bb400de8a1b8ce1082
Hleb Rubanau <g.rubanau@gmail.com>
2026-05-18 10:15:55 +0200

    chore(test/helpers/codesnippets): linter

--- tests/helpers/codesnippets.lua
+++ tests/helpers/codesnippets.lua
@@ -1,24 +1,27 @@
 function mock_func_snippet(name, num_lines)
   num_lines = num_lines or 3
   assert(num_lines >= 3, "minimum number of lines is 3")
-  
+
   local lines = {}
-  
+
   lines[#lines + 1] = string.format("function %s()", name)
-  lines[#lines + 1] = string.format('  print("called %s(), a function of %d lines")', name, num_lines)
-  
+  lines[#lines + 1] = string.format(
+    '  print("called %s(), a function of %d lines")',
+    name,
+    num_lines
+  )
+
   for i = 1, num_lines - 3 do
     lines[#lines + 1] = string.format("  -- line %d", i)
   end
 
   lines[#lines + 1] = "end"
-  
+
   return string.unlines(lines)
 end
 
-
 function snippets_to_code(...)
-  local snippets = {...}
+  local snippets = { ... }
   local metadata = {}
   local cursor = 0
 
@@ -26,12 +29,12 @@ function snippets_to_code(...)
     cursor = cursor + 1
     local slines = string.lines(snippet)
     metadata[#metadata + 1] = {
-      pos   = i,
-      len   = #slines,
+      pos = i,
+      len = #slines,
       start = cursor,
-      ["end"] = cursor + #slines
+      ["end"] = cursor + #slines,
     }
-    cursor = cursor + #slines -1
+    cursor = cursor + #slines - 1
   end
 
   return string.unlines(snippets), metadata
